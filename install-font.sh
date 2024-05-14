ead1e9711f6fc66110ceb14f36e8fbe89a63e358
amirtuval <amirtu@gmail.com>
2024-05-14 09:11:00 +0000

    prerequisites doc

---
+++ install-font.sh
@@ -0,0 +1,5 @@
+sudo apt install fontconfig
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d ~/.local/share/fonts/
+fc-cache -fv
