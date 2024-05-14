b5973ed2d6aef818c77ba0cf22f0caea87ea3358
amirtuval <amirtu@gmail.com>
2024-05-14 15:40:59 +0000

    Add mappings for document and workspace symbols

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>ts", "<cmd> Telescope lsp_document_symbols <CR>", { desc = "Telescope Find in document symbols" })
+map("n", "<leader>tws", "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", { desc = "Telescope Find in workspace symbols" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
